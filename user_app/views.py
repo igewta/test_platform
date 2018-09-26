cd133cf139516a17ebace1bf0a03976e7a7eb7a9
igewta <igewta@foxmail.com>
2018-09-26 23:50:31 +0800

    完善登陆

--- user_app/views.py
+++ user_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import auth
 
 # Create your views here.
 
@@ -6,10 +7,16 @@ def index(requests):
 	return render(requests,'index.html')
 
 def login_action(requests):
-	if requests.method == "GET":
-		username = requests.GET.get("username")
-		password = requests.GET.get("password")
+	if requests.method == "POST":
+		username = requests.GET.get("username","")
+		password = requests.GET.get("password","")
 
 		if username == " " or password =="":
 			return render(requests,"index.html",{'error':'用户名或者密码错误'})
-			
+		
+		user = auth.authenticate(username=username,password=password)
+		if user is not None:
+			auth.login(requests,user)
+			return render(requests,'project_manage.html',"登陆成功")
+		else:
+			return render(requests,'index.html',"用户名或密码错误")
