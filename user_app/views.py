6ff7cf596fb81568a87cf17daa8a3ffefe5fde44
igewta <igewta@foxmail.com>
2018-09-27 14:28:03 +0800

    完善登录

--- user_app/views.py
+++ user_app/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
+from django.contrib import auth
 
 # Create your views here.
 
 def index(requests):
-
 	return render(requests,'index.html')
 
 def login_action(requests):
 	if requests.method == "POST":
-		username = requests.GET.get("username","")
-		password = requests.GET.get("password","")
+		username = requests.POST.get("username"," ")
+		password = requests.POST.get("password"," ")
 
-		if username == " " or password =="":
-			return render(requests,"index.html",{'error':'用户名或者密码错误'})
-
-		if True:
-			pass
+		if username == "" or password =="":
+			return render(requests,"index.html",{'error':'用户名或者密码为空'})
 		
+		user = auth.authenticate(username=username,password=password)
+
+		if user is not None:
+			auth.login(requests,user)
+			return render(requests,"project_manage.html")
+		else:
+			return render(requests,'index.html',{'error':'用户名或者密码错误'})
+		
+
